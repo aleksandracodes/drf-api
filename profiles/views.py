48ccfeb661e89631ee9467f02f4ece65822c5d19
Aleksandra <a.haniok@gmail.com>
2022-05-26 07:40:16 +0000

    Refactor Post and Profile Views

--- profiles/views.py
+++ profiles/views.py
@@ -1,57 +1,22 @@
-from django.http import Http404
-from rest_framework import status # import for the put method
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
+class ProfileList(generics.ListAPIView):
     """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
+    List all profiles.
+    No create view as profile creation is handled by django signals.
     """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
 
 
-class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer # render a form based on the fields defined in the ProfileSerializer
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        # 1. fetch the profile by id
-        # 2. serialize the Profile model instance
-        # 3. return serializer data in the response
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        # 1. fetch the profile by id
-        # 2. call serializer with the profile and request data
-        # 3. if data is valid, save and return the instance
-        # 4. if data is invalid, return the 404 error
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data,  context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
