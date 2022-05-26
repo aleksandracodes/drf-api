1b98eb674e553faef9b915d07e28b82d3cf69046
Aleksandra <a.haniok@gmail.com>
2022-05-26 07:44:14 +0000

    Fix typo in the urls

--- followers/urls.py
+++ followers/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from likes import views
+from followers import views
 
 urlpatterns = [
     path('followers/', views.FollowerList.as_view()),
-    path('followers/ < int:pk > /', views.FollowerDetail.as_view()),
+    path('followers/<int:pk>/', views.FollowerDetail.as_view())
 ]
